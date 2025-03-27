1c1827b67ab5e5b3a24f53bf7de255ed034813c2
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-03-27 14:23:39 +0200

    register + verify

--- src/Modules/Authentication/Verify-Account/VerifyAccount.tsx
+++ src/Modules/Authentication/Verify-Account/VerifyAccount.tsx
@@ -1,8 +1,63 @@
+import { Form ,Col, Button} from "react-bootstrap"
+import { useForm } from "react-hook-form"
+import { useLocation, useNavigate } from "react-router-dom"
+import { publicAxiosInstance } from "../../../Services/Axiosinstanc"
+import { USERS_URLS } from "../../../Services/Urls"
+import { toast } from "react-toastify"
+import { Code_Validation, Email_Validation } from "../../../Services/Validation"
+import { AxiosError } from "axios"
+
 
 const VerifyAccount = () => {
+  const navigate=useNavigate()
+  const {state}=useLocation()
+  interface IVerfyData{
+    email:string
+    ,code:string
+  }
+  const {register,formState:{errors,isSubmitting},handleSubmit}=useForm<IVerfyData>({
+    mode:'onChange',
+    defaultValues:{email:state?.email}
+  })
+
+
+
+const onSubmit=async(values:IVerfyData)=>{
+console.log(values)
+try {
+  const {data}=await publicAxiosInstance.put(USERS_URLS.VERIFY_USER,values)
+  toast.success(data?.message)
+  navigate("/login")
+} catch (error) {
+  if (error instanceof AxiosError) {
+    toast.error(error.response?.data?.message || "Something Went Wrong");
+  } else if (error instanceof Error) {
+    toast.error(error.message || "Something Went Wrong");
+  } else {
+    toast.error("Something Went Wrong");
+  }
+}
+}
   return (
-    <div>
-      <h1>VerifyAccount</h1>
+    <div className="register">
+<div>  <span className="text-white">welcome to PMS</span>
+<h3 className="heading">Verify Account</h3></div>
+<form onSubmit={handleSubmit(onSubmit)}>
+<Form.Group as={Col} controlId="formGridEmail" className="mb-3">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control type="email" placeholder="Enter Your Email" className="input-register input-register-border"  {...register('email',Email_Validation)}/>
+          {errors.email && <p className="text-error mt-2">{errors.email.message}</p>}
+        </Form.Group>
+<Form.Group as={Col} controlId="formGridEmail" className="mb-3">
+          <Form.Label>OTP Verification</Form.Label>
+          <Form.Control type="text" placeholder="Enter Verification" className="input-register input-register-border"  {...register('code',Code_Validation)}/>
+          {errors.code && <p className="text-error mt-2">{errors.code.message}</p>}
+        </Form.Group>
+        <div className="w-75 mx-auto"><Button disabled={isSubmitting} className="save-btn w-100 mt-4 rounded-pill" type="submit">{isSubmitting?<>
+          <i className="fas fa-spinner fa-spin me-2"></i>
+        <span>Saving...</span>
+        </>:"Save"}</Button></div>
+</form>
     </div>
   )
 }
--- src/Modules/Shared/AuthLayout/AuthLayout.tsx
+++ src/Modules/Shared/AuthLayout/AuthLayout.tsx
@@ -4,9 +4,11 @@ import Logo from '../../../assets/logo.png'
 const AuthLayout = () => {
   return (
     <>
-       <div className="auth-container">
+       <div className="auth-container overflow-y-auto">
+        <div className="bg-top"></div>
+        <div className="bg-bottom"></div>
         <Container fluid>
-        <Row className="vh-100 justify-content-center align-items-center">
+        <Row className="vh-100 justify-content-center align-items-center position-relative">
           <Col sm={6}>
           <div className="text-center mb-4">
           <img src={Logo} width={250} alt="Logo"/>
