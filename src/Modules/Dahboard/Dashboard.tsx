23ac9727fcee9f5d010090cd71e7831788532efb
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-04-01 20:12:45 +0200

    add name to dashboard

--- src/Modules/Dahboard/Dashboard.tsx
+++ src/Modules/Dahboard/Dashboard.tsx
@@ -3,7 +3,6 @@ import { AuthContext } from "../../context/AuthContext"
 
 export default function Dashboard() {
   const {loginData}=useContext(AuthContext)
-
   return <div className="home-bg d-flex align-items-center">
 <div className="mx-3"><h3 className="title">Welcome <span  >{loginData?.userName}</span></h3>
 <p className="text-white">You can add project and assign tasks to your team</p></div>
