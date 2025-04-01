cb9d4f6a74e15db11447e5d53654a753cd6d4ef9
Ebtehal18 <ebtehallelsayed18@gmail.com>
2025-04-01 18:07:20 +0200

    fix import

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import AuthContextProvider from './context/authContext.tsx'
+import AuthContextProvider from './context/AuthContext.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthContextProvider>
