9153cbda125076b26abc76ddb437ddb2a470284d
yaratarek100 <yara.tarek.300@gmail.com>
2025-04-10 13:30:16 +0200

    step back

--- src/Modules/Shared/Interfaces/projectInterface.ts
+++ src/Modules/Shared/Interfaces/projectInterface.ts
@@ -1,59 +1,60 @@
 export interface Task {
-  id: number;
-  title: string;
-  description: string;
-  status: "ToDo" | "InProgress" | "Done";
-  creationDate: string;
-  modificationDate: string;
-}
+    id: number;
+    title: string;
+    description: string;
+    status: "ToDo" | "InProgress" | "Done"; 
+    creationDate: string;
+    modificationDate: string;
+  }
+  
+  // export interface Project {
+  //   id: number;
+  //   title: string;
+  //   description: string;
+  //   creationDate: string;
+  //   modificationDate: string;
+  //   task: Task[];
+  // }
 
-// export interface Project {
-//   id: number;
-//   title: string;
-//   description: string;
-//   creationDate: string;
-//   modificationDate: string;
-//   task: Task[];
-// }
+export  interface ProjectManager {
+    id: number;
+    userName: string;
+    imagePath: string;
+    email: string;
+    password: string;
+  }
+  
+ export interface Project {
+    id: number;
+    title: string;
+    description: string;
+    creationDate: string;
+    modificationDate: string;
+    manager: ProjectManager;
+    task: any[];
+  }
+  
+ export interface ViewProjectProps {
+    show: boolean;
+    handleClose: () => void;
+    showProjectId: number;
+  }
+  
+  
+  export interface PaginatedProjectsResponse {
+    pageNumber: number;
+    pageSize: number;
+    data: Project[];
+    totalNumberOfRecords: number;
+    totalNumberOfPages: number;
+  }
 
-export interface ProjectManager {
-  id: number;
-  userName: string;
-  imagePath: string;
-  email: string;
-  password: string;
-}
-
-export interface Project {
-  id: number;
-  title: string;
-  description: string;
-  creationDate: string;
-  modificationDate: string;
-  manager: ProjectManager;
-  task: any[];
-}
-
-export interface ViewProjectProps {
-  show: boolean;
-  handleClose: () => void;
-  showProjectId: number;
-}
-
-export interface PaginatedProjectsResponse {
-  pageNumber: number;
-  pageSize: number;
-  data: Project[];
-  totalNumberOfRecords: number;
-  totalNumberOfPages: number;
-}
-
-export interface TProjectData {
-  title: string;
-  description: string;
-}
-export interface PaginatedViewProjectsResponse {
-  show: boolean;
-  handleClose: () => void;
-  showProjectId: number;
-}
+  export interface TProjectData{
+    title:string,
+   description: string
+ }
+ export interface PaginatedViewProjectsResponse{
+    show:boolean,
+    handleClose:()=>void,
+    showProjectId:number
+  }
