bee2bb65a830ad05122dc464db5bbe7c9d463bc3
yaratarek100 <yara.tarek.300@gmail.com>
2025-05-04 13:26:55 +0300

    .

--- src/Modules/Projects/ProjectsList/ProjectsList.tsx
+++ src/Modules/Projects/ProjectsList/ProjectsList.tsx
@@ -9,7 +9,7 @@ import { AuthContext } from "../../../context/AuthContext";
 import Pagination from "../../Shared/Pagination";
 import Actions from "../../Shared/Actions/Actions";
 import Loading from "../../Shared/Loading/Loading";
-import { PaginatedProjectsResponse } from "../../Shared/Interfaces/projectInterface";
+import { PaginatedProjectsResponse } from "../../Shared/Interfaces/ProjectInterface";
 import DeleteConfirmation from "../../Shared/DeleteConfirmation/DeleteConfirmation";
 
 const ProjectsList = () => {
